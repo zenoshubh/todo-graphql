7f53e2390337910fe0e17e75c8b58fd71c78abc6
Shubh Verma <shubhverma2003@gmail.com>
2025-06-13 19:11:42 +0530

    feat: set up Apollo Client with authentication context for GraphQL integration

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+import { AuthProvider } from "./context/AuthContext.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const httpLink = createHttpLink({
+  uri: "http://localhost:8000",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ApolloProvider client={client}>
+      <AuthProvider>
+      <App />
+      </AuthProvider>
+    </ApolloProvider>
+  </StrictMode>
+);
