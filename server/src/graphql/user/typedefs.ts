bfae88914e31e6bcda689cbe1e6c6173b82ee9e5
Shubh Verma <shubhverma2003@gmail.com>
2025-06-13 11:53:37 +0530

    feat: add GraphQL type definitions for User model and authentication operations

---
+++ server/src/graphql/user/typedefs.ts
@@ -0,0 +1,40 @@
+export const typeDefs = `#graphql
+  # User type definition
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    createdAt: String!
+  }
+
+  # Authentication response types
+  type AuthResponse {
+    success: Boolean!
+    message: String!
+    token: String
+    user: User
+  }
+
+  # Input types
+  input RegisterInput {
+    name: String!
+    email: String!
+    password: String!
+  }
+
+  input LoginInput {
+    email: String!
+    password: String!
+  }
+
+  type Query {
+    # Get current user info (requires authentication)
+    me: User
+  }
+
+  type Mutation {
+    # Authentication mutations
+    register(input: RegisterInput!): AuthResponse!
+    login(input: LoginInput!): AuthResponse!
+  }
+`;
